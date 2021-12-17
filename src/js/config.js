beef591e1fe214206afcbd1220077f45785b327b
codewithshahan <programmingwithshahan@gmail.com>
2021-12-17 18:28:59 +0600

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '36cdcc1e-f278-45a8-a77a-6af5bd74bf2e';
+export const MODAL_CLOSE_SEC = 2.5;
